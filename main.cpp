85f2f6d55ddff80f65f26f56a404c2e835420fca
Catalina-Dumitru <127409988+Catalina-Dumitru@users.noreply.github.com>
2023-04-07 12:45:36 +0300

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,33 +1,49 @@
 #include <iostream>
-#include<fstream>
+
 using namespace std;
 
 int main()
 {
-    long long x,i,v[13];
-    ifstream f("date.in");
-    ofstream g("date.out");
-    cout<<5030816046216<<endl;
-    while(f>>x)
-        {
+int n,v[100];
+cout<<"Numarul de elememnte din vector";cin>>n;
+for(int i=0;i<n;i++)
+{
 
-        for(i=0;i<13;i++)
-        {
-            v[i]=x%10;
-            x=x/10;
-        }
-        for(i=0;i<=6;i++){
-            swap(v[i],v[12-i]);}
-        if(v[0]==1)
-            cout<<"Sexul persoanei este masculin iar anul nasterii este 19";
-        else if(v[0]==2)
-           cout<<"Sexul persoanei este feminin iar anul nasterii este 19";
-        else if(v[0]==6)
-            cout<<"Sexul persoanei este feminim iar anul nasterii este 20";
-        else if(v[0]==5)
-        cout<<"Sexul persoanei este masculin iar anul nasterii este 20";
-        cout<<v[1]<<v[2]<<" ";
-        cout<<"luna nasterii este a-"<<v[4]<<"-a ziua"<<" "<<v[5]<<v[6]<<endl;
-        }
-        return 0;
+cout<<"Elementul de pe pozitia"<<i<<" ";
+cin>>v[i];
 }
+int c,ins;
+cout<<"Adauga un numar pe pozitia:";cin>>c;
+cout<<"Adauga numarul1:";cin>>ins;
+if(c<n)
+{
+    n++;
+    for(int i=n-1;i>=c;i--)
+        v[i]=v[i-1];
+    v[c]=ins;
+    for(int i=0;i<n;i++)
+    cout<<v[i]<<" ";
+}
+else if(c==n)
+{
+    v[n]=ins;
+    n++;
+    for(int i=0;i<n;i++)
+    cout<<v[i]<<" ";
+
+
+}
+else
+ {
+     for(int i=n;i<c;i++)
+        v[i]=0;
+     v[c]=ins;
+     n=c+1;
+     for(int i=0;i<n;i++)
+        cout<<v[i]<<" ";
+
+}
+}
+
+
+
